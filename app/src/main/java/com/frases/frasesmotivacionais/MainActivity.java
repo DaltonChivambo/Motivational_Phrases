c664cc349961fbb901d1c464b2f0485da94e00f0
dalto <daltonchivambo@gmail.com>
2022-10-08 14:27:03 +0200

    Add event on btn_gerarFrases

--- app/src/main/java/com/frases/frasesmotivacionais/MainActivity.java
+++ app/src/main/java/com/frases/frasesmotivacionais/MainActivity.java
@@ -1,6 +1,7 @@
 package com.frases.frasesmotivacionais;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -8,18 +9,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView frases;
-    private Button gerarFrases;
+    private TextView tv_frases;
+    private Button btn_gerarFrases;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        frases = findViewById(R.id.frases);
-        gerarFrases = findViewById(R.id.gerarFrases);
+        tv_frases = findViewById(R.id.tv_frases);
+        btn_gerarFrases = findViewById(R.id.btn_gerarFrases);
 
         getSupportActionBar().hide(); //Esconder Barra de cima (Action Bar)
+
+        //evento no button
+        btn_gerarFrases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String [] frasesMotivacionais = {"aaaaa","bbbb","ccccc"};
+
+                tv_frases.setText(frasesMotivacionais[0]);
+            }
+        });
 
     }
 }
